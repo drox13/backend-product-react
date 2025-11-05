1c9a089f01c25e492d1a2a985693bea188bce410
Dario Barón <dariobaron@software-genesis.com>
2025-11-05 15:16:45 -0500

    conexion con frontend

--- src/main/java/com/dario/curso/springboot/backend/backend_products/repositories/ProductRepository.java
+++ src/main/java/com/dario/curso/springboot/backend/backend_products/repositories/ProductRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.dario.curso.springboot.backend.backend_products.entities.Product;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:5173/") // url del front
+@CrossOrigin(origins = "http://localhost:5173/") // url del front para que no moleste el CORS
 @RepositoryRestResource(path = "products")
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
