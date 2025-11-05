1995d04b6b21d598f79ca0193bc7b29e30a83a7c
Dario Barón <dariobaron@software-genesis.com>
2025-11-05 10:35:01 -0500

    backend

---
+++ src/main/java/com/dario/curso/springboot/backend/backend_products/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.dario.curso.springboot.backend.backend_products.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.dario.curso.springboot.backend.backend_products.entities.Product;
+
+@RepositoryRestResource(path = "products")
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+}
